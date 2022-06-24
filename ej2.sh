6df9a6590df768a62b32bc02ea5583103eb90f06
Agustín Roca <aroca@itba.edu.ar>
2022-06-24 17:14:52 -0300

    Skip update ej2

--- ej2.sh
+++ ej2.sh
@@ -3,41 +3,49 @@
 Help()
 {
    # Display Help
-   echo "FASTA -> BLAST."
+   echo "usage: ej2.sh [-h] [-i FASTA_FILE] [-o OUTPUT_BLAST_FILE] [-r] [-s]"
    echo
-   echo "Syntax: ej2.sh [-h] [-i FASTA_FILE] [-o OUTPUT_BLAST_FILE] [-r]"
-   echo "options:"
-   echo "h                      Print this Help."
-   echo "i FASTA_FILE           Input FASTA file (default = sequences/results/protein.fasta)"
-   echo "o OUTPUT_BLAST_FILE    Output BLAST XML file (default = sequences/results/blast.out)"
-   echo "r                      Remote run. If not present, run locally"
+   echo "Ejercicio 2. FASTA -> BLAST."
+   echo
+   echo "optional arguments:"
+   echo " -h                      show this help message and exit"
+   echo " -i FASTA_FILE           Input FASTA file (default = sequences/results/protein.fasta)"
+   echo " -o OUTPUT_BLAST_FILE    Output BLAST XML file (default = sequences/results/blast.out)"
+   echo " -r                      Remote run. If not present, run locally"
+   echo " -s                      Skip update (only relevant in local run). If not present, will check for updates of swissprot in NCBI"
 }
 
 input="sequences/results/protein.fasta"
 output="sequences/results/blast.out"
 remote=false
-while getopts ":hi:o::r" flag
+update=true
+while getopts ":hi:o::r:s" flag
 do
     case "${flag}" in
-        h) # display Help
+        h)
             Help
             exit;;
         i) input=${OPTARG};;
         o) output=${OPTARG};;
         r) remote=true;;
+        s) update=false;;
         
     esac
 done
+
 
 if $remote
 then
     echo "Running remote BLAST"
     ./blast/bin/blastp -db swissprot -query $input -out $output -outfmt 5 -remote
 else
-    cd blast/data
-    echo "Checking updates on swissprot..."
-    ../bin/update_blastdb.pl --passive --decompress swissprot
-    cd ../..
+    if $update
+    then
+        cd blast/data
+        echo "Checking updates on swissprot..."
+        ../bin/update_blastdb.pl --passive --decompress swissprot
+        cd ../..
+    fi
     echo "Running local BLAST..."
     ./blast/bin/blastp -db blast/data/swissprot -query $input -out $output -outfmt 5
     echo "Done"
