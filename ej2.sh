aa647e26dc8e47ed6c848de82c6464f582cf7504
Agustín Roca <aroca@itba.edu.ar>
2022-06-24 14:23:49 -0300

    Ej2 remote via BASH

---
+++ ej2.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+Help()
+{
+   # Display Help
+   echo "FASTA -> BLAST."
+   echo
+   echo "Syntax: ej2.sh [-h] [-i FASTA_FILE] [-o OUTPUT_BLAST_FILE] [-r]"
+   echo "options:"
+   echo "h                      Print this Help."
+   echo "i FASTA_FILE           Input FASTA file (default = sequences/results/protein.fasta)"
+   echo "o OUTPUT_BLAST_FILE    Output BLAST XML file (default = sequences/results/blast.out)"
+   echo "r                      Remote run. If not present, run locally"
+}
+
+input="sequences/results/protein.fasta"
+output="sequences/results/blast.out"
+remote=false
+while getopts ":hi:o::r" flag
+do
+    case "${flag}" in
+        h) # display Help
+            echo "Hola"
+            Help
+            exit;;
+        i) input=${OPTARG};;
+        o) output=${OPTARG};;
+        r) remote=true;;
+        
+    esac
+done
+
+if $remote
+then
+    echo "Running remote BLAST"
+    ./blast/bin/blastp -db swissprot -query $input -out $output -outfmt 5 -remote
+else
+    echo "Running local BLAST"
+    ./blast/bin/blastp -db blast/data/swissprot -query $input -out $output -outfmt 5
+fi
--- ej2_local.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# ./blast/bin/blastp -db blast/data/swissprot -query sequences/results/protein.fasta -out sequences/results/blast.out -outfmt 5
-./blast/bin/blastp -db blast/data/swissprot -query $1 -out $2 -outfmt 5
